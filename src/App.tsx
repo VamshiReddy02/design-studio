79a5cb0dae89a7a6ca08c7de5a44311858295dd8
VamshiReddy02 <vamshiproject02@gmail.com>
2025-05-10 14:13:16 +0530

    initial setup
    Signed-off-by: VamshiReddy02 <vamshiproject02@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import Blogs from "./sections/Blogs"
+import Footer from "./sections/Footer"
+import Hero from "./sections/Hero"
+import Introduction from "./sections/Introduction"
+import Logoticker from "./sections/LogoTicker"
+import Navbar from "./sections/Navbar"
+import Projects from "./sections/Projects"
+import RoadMap from "./sections/RoadMap"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Introduction />
+      <Projects />
+      <Logoticker />
+      <RoadMap />
+      <Blogs />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/sections/LogoTicker.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logoticker = () => {
+  return (
+    <div>Logoticker</div>
+  )
+}
+
+export default Logoticker
